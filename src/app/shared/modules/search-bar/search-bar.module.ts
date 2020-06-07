950a91d07f29eab2173da4cdd0e7937469b6c782
tuvetula <romain.marcant@gmail.com>
2020-06-07 17:31:49 +0200

    searchBar creation component

---
+++ src/app/shared/modules/search-bar/search-bar.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { SearchBarComponent } from '../../components/search-bar/search-bar.component';
+
+
+
+@NgModule({
+  declarations: [SearchBarComponent],
+  imports: [
+    CommonModule
+  ],
+  exports:[
+    CommonModule,
+    SearchBarComponent
+  ]
+})
+export class SearchBarModule { }
