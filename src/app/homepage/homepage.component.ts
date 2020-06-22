ae9bda0c61983041337fa43f7bee089591499825
tuvetula <romain.marcant@gmail.com>
2020-06-22 02:58:58 +0200

    homepage.ts

--- src/app/homepage/homepage.component.ts
+++ src/app/homepage/homepage.component.ts
@@ -9,9 +9,7 @@ import { AppliService } from '../shared/services/Menus/appli.service';
 export class HomepageComponent implements OnInit {
   public nameSection: string = "Accueil";
 
-  constructor(
-    private appliService: AppliService,
-  ) {}
+  constructor(private appliService: AppliService,) {}
 
   ngOnInit(): void {
      //On paramètre la section en cours
